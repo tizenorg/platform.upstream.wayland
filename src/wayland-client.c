dbcd7f1cd296890268c535def1d8b77f0aef79af
Boram Park <boram1288.park@samsung.com>
2015-07-10 16:11:03 +0900

    fix fd polling bug
       Workaround solution until resolving the below bug.
       https://bugs.freedesktop.org/show_bug.cgi?id=91273
    
       Sometimes sub-thread reads all events from display fd too fast for
       main-thread to awake from poll waiting. If main-thread's queues is not
       empty, we will forcely awake main-thread from poll waiting. When
       main-thread awakes, queues will be dispatched.
    
    Change-Id: Ibc4d6b2b0cf433a4ec5f6d7e69336ac0d1cc0be4

--- src/wayland-client.c
+++ src/wayland-client.c
@@ -38,11 +38,14 @@
 #include <fcntl.h>
 #include <poll.h>
 #include <pthread.h>
+#include <sys/syscall.h>
 
 #include "wayland-util.h"
 #include "wayland-os.h"
 #include "wayland-client.h"
 #include "wayland-private.h"
+
+#define FIX_WAYLAND_CLIENT_FD_POOLING_BUG_IN_MULTITHREAD
 
 /** \cond */
 
@@ -71,6 +74,11 @@ struct wl_global {
 struct wl_event_queue {
 	struct wl_list event_list;
 	struct wl_display *display;
+
+#ifdef FIX_WAYLAND_CLIENT_FD_POOLING_BUG_IN_MULTITHREAD
+	pid_t tid;
+	struct wl_list link;
+#endif
 };
 
 struct wl_display {
@@ -103,6 +111,11 @@ struct wl_display {
 	int reader_count;
 	uint32_t read_serial;
 	pthread_cond_t reader_cond;
+
+#ifdef FIX_WAYLAND_CLIENT_FD_POOLING_BUG_IN_MULTITHREAD
+	pid_t tid;
+	struct wl_list queue_list;
+#endif
 };
 
 /** \endcond */
@@ -215,6 +228,10 @@ wl_event_queue_init(struct wl_event_queue *queue, struct wl_display *display)
 {
 	wl_list_init(&queue->event_list);
 	queue->display = display;
+
+#ifdef FIX_WAYLAND_CLIENT_FD_POOLING_BUG_IN_MULTITHREAD
+	wl_list_insert(&display->queue_list, &queue->link);
+#endif
 }
 
 static void
@@ -294,6 +311,9 @@ wl_event_queue_destroy(struct wl_event_queue *queue)
 
 	pthread_mutex_lock(&display->mutex);
 	wl_event_queue_release(queue);
+#ifdef FIX_WAYLAND_CLIENT_FD_POOLING_BUG_IN_MULTITHREAD
+	wl_list_remove(&queue->link);
+#endif
 	free(queue);
 	pthread_mutex_unlock(&display->mutex);
 }
@@ -315,7 +335,13 @@ wl_display_create_queue(struct wl_display *display)
 	if (queue == NULL)
 		return NULL;
 
+#ifdef FIX_WAYLAND_CLIENT_FD_POOLING_BUG_IN_MULTITHREAD
+	pthread_mutex_lock(&display->mutex);
+#endif
 	wl_event_queue_init(queue, display);
+#ifdef FIX_WAYLAND_CLIENT_FD_POOLING_BUG_IN_MULTITHREAD
+	pthread_mutex_unlock(&display->mutex);
+#endif
 
 	return queue;
 }
@@ -822,6 +848,12 @@ wl_display_connect_to_fd(int fd)
 
 	memset(display, 0, sizeof *display);
 
+#ifdef FIX_WAYLAND_CLIENT_FD_POOLING_BUG_IN_MULTITHREAD
+	/* get id of main-thread */
+	display->tid = syscall(SYS_gettid);
+	wl_list_init(&display->queue_list);
+#endif
+
 	display->fd = fd;
 	wl_map_init(&display->objects, WL_MAP_CLIENT_SIDE);
 	wl_event_queue_init(&display->default_queue, display);
@@ -1306,6 +1338,27 @@ wl_display_read_events(struct wl_display *display)
 {
 	int ret;
 
+#ifdef FIX_WAYLAND_CLIENT_FD_POOLING_BUG_IN_MULTITHREAD
+		/* Workaround solution until resolving the below bug.
+		 * https://bugs.freedesktop.org/show_bug.cgi?id=91273
+		 * Sometimes sub-thread reads all events from display fd too fast for
+		 * main-thread to awake from poll waiting. If main-thread's queues is not
+		 * empty, we will forcely awake main-thread from poll waiting. When
+		 * main-thread awakes, queues will be dispatched.
+		 */
+		if (display->tid != syscall(SYS_gettid)) {
+			struct wl_event_queue *temp;
+			wl_list_for_each(temp, &display->queue_list, link) {
+				if (temp->tid == 0 || temp->tid == display->tid) {
+					if (!wl_list_empty(&temp->event_list)) {
+						struct wl_callback *callback = wl_display_sync(display);
+						break;
+					}
+				}
+			}
+		}
+#endif
+
 	pthread_mutex_lock(&display->mutex);
 
 	if (display->last_error) {
@@ -1376,6 +1429,14 @@ wl_display_prepare_read_queue(struct wl_display *display,
 	int ret;
 
 	pthread_mutex_lock(&display->mutex);
+
+#ifdef FIX_WAYLAND_CLIENT_FD_POOLING_BUG_IN_MULTITHREAD
+	/* if queue->tid is not same with display->tid, then we can guess this queue
+	 * will be handled in sub-thread.
+	 */
+	if (queue->tid == 0)
+		queue->tid = syscall(SYS_gettid);
+#endif
 
 	if (!wl_list_empty(&queue->event_list)) {
 		errno = EAGAIN;
