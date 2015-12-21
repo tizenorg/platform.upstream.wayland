4acb229f700e46b99d258395efc447a4ea347dec
Ju Yeon Lee <juyeonne.lee@samsung.com>
2015-12-21 20:11:22 +0900

    server: Add new api to get wayland clients' resource
    allows us to get connected clients and their resources
    could be utilized more efficient sever resource usage and debug resource leakage
    added tests 'clients-resource-test' for presenting howto.
    
    Change-Id: I53647d3095b51d8f435eaaad0fedb6e13dedfb3b

--- src/wayland-server.c
+++ src/wayland-server.c
@@ -541,6 +541,20 @@ wl_client_get_object(struct wl_client *client, uint32_t id)
 	return wl_map_lookup(&client->objects, id);
 }
 
+WL_EXPORT struct wl_client *
+wl_client_from_link(struct wl_list *link)
+{
+	struct wl_client *client;
+
+	return wl_container_of(link, client, link);
+}
+
+WL_EXPORT struct wl_list *
+wl_client_get_link(struct wl_client *client)
+{
+	return &client->link;
+}
+
 WL_EXPORT void
 wl_client_post_no_memory(struct wl_client *client)
 {
@@ -596,6 +610,12 @@ WL_EXPORT uint32_t
 wl_resource_get_id(struct wl_resource *resource)
 {
 	return resource->object.id;
+}
+
+WL_EXPORT const char *
+wl_resource_get_name(struct wl_resource *resource)
+{
+   return resource->object.interface->name;
 }
 
 WL_EXPORT struct wl_list *
@@ -1453,6 +1473,19 @@ wl_display_add_shm_format(struct wl_display *display, uint32_t format)
 	if (p != NULL)
 		*p = format;
 	return p;
+}
+
+
+WL_EXPORT struct wl_list *
+wl_display_get_client_list(struct wl_display *display)
+{
+	return &display->client_list;
+}
+
+WL_EXPORT struct wl_map *
+wl_client_get_resources(struct wl_client *client)
+{
+	return &client->objects;
 }
 
 /**
